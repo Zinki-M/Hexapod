b3973c84cc1bb69e10a82f45028c21042c049888
Zinki <z@a>
2022-01-07 22:26:01 +0100

    #BF fix

--- Custom/Poses.py
+++ Custom/Poses.py
@@ -22,11 +22,11 @@ def curledPosition():
     try:
         for i in range(32):
             if (i == 10 or i == 13 or i == 31):
-                S.setServoAngle(i,0)
+                servo.setServoAngle(i,0)
             elif (i == 18 or i == 21 or i == 27):
-                S.setServoAngle(i,180)
+                servo.setServoAngle(i,180)
             else:
-                S.setServoAngle(i,90)
+                servo.setServoAngle(i,90)
         time.sleep(1)
 
         for i in range(60):
