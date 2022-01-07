a24f6c57a3e3bc78c2acfce5d9ab215d001f98a7
Zinki <z@a>
2022-01-07 22:24:56 +0100

    #BF fix angles?

--- Custom/Poses.py
+++ Custom/Poses.py
@@ -13,20 +13,20 @@ def zeroPosition():
             S.setServoAngle(i,180)
         else:
             S.setServoAngle(i,90)
-    time.sleep(3)
+    time.sleep(1)
 
 
 def curledPosition():
     print("Curl up")
     servo=Servo.Servo()     
     try:
-        
-        servo.setServoAngle(15,90)
-        servo.setServoAngle(12,90)
-        servo.setServoAngle(9, 90)
-        servo.setServoAngle(16,90)
-        servo.setServoAngle(19,90)
-        servo.setServoAngle(22,90)
+        for i in range(32):
+        if (i == 10 or i == 13 or i == 31):
+            S.setServoAngle(i,0)
+        elif (i == 18 or i == 21 or i == 27):
+            S.setServoAngle(i,180)
+        else:
+            S.setServoAngle(i,90)
         time.sleep(1)
 
         for i in range(60):
@@ -38,12 +38,12 @@ def curledPosition():
             servo.setServoAngle(23,90-i)
             time.sleep(0.005)
         for i in range(180):
-            servo.setServoAngle(13,180-i)
-            servo.setServoAngle(10,180-i)
-            servo.setServoAngle(31,180-i)
-            servo.setServoAngle(18,i)
-            servo.setServoAngle(21,i)
-            servo.setServoAngle(27,i)
+            servo.setServoAngle(13,180+i)
+            servo.setServoAngle(10,180+i)
+            servo.setServoAngle(31,180+i)
+            servo.setServoAngle(18,180-i)
+            servo.setServoAngle(21,180-i)
+            servo.setServoAngle(27,180-i)
             time.sleep(0.005)
     except KeyboardInterrupt:
         print ("\ninterrrupted")
