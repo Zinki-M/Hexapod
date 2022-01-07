dea1ce4ffaf97e914718f6963e11c49419e200c5
Zinki <z@a>
2022-01-07 22:11:44 +0100

    #IM switch poses

--- Custom/Poses.py
+++ Custom/Poses.py
@@ -27,7 +27,40 @@ def curledPosition():
     time.sleep(3)
 
 
+def standingPosition():
+    servo=Servo.Servo()
+    try:
+        
+        for i in range(50):
+            servo.setServoAngle(15,90+i)
+            servo.setServoAngle(12,90+i)
+            servo.setServoAngle(9,90+i)
+            servo.setServoAngle(16,90+i)
+            servo.setServoAngle(19,90+i)
+            servo.setServoAngle(22,90+i)
+            time.sleep(0.005)
+        for i in range(60):
+            servo.setServoAngle(14,90+i)
+            servo.setServoAngle(11,90+i)
+            servo.setServoAngle(8,90+i)
+            servo.setServoAngle(17,90-i)
+            servo.setServoAngle(20,90-i)
+            servo.setServoAngle(23,90-i)
+            time.sleep(0.005)
+        for i in range(120):
+            servo.setServoAngle(13,i)
+            servo.setServoAngle(10,i)
+            servo.setServoAngle(31,i)
+            servo.setServoAngle(18,180-i)
+            servo.setServoAngle(21,180-i)
+            servo.setServoAngle(27,180-i)
+            time.sleep(0.005)
+        print ("\nEnd of program")      
+    except KeyboardInterrupt:
+        print ("\nEnd of program")
 
 if __name__ == '__main__':
+    zeroPosition()
+    standingPosition()
     zeroPosition()
     curledPosition()
