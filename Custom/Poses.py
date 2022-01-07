d56842a86b43ebfe56dc7be27e7976503bb3976d
Zinki <z@a>
2022-01-07 22:08:27 +0100

    #BF fix imports

--- Custom/Poses.py
+++ Custom/Poses.py
@@ -1,7 +1,7 @@
 from os import sys, path
 sys.path.append(path.join(path.dirname(path.dirname(path.abspath(__file__))),"Server"))
 import Servo
-
+import time
 
 def zeroPosition():
     S=Servo.Servo()     
