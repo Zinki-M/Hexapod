8b0702ae8b3ad9ccd6bbebaa881aa15558947c16
Zinki <z@a>
2022-01-07 17:43:13 +0100

    #IM catch exception in battery read

--- Server/ADCDevice.py
+++ Server/ADCDevice.py
@@ -49,5 +49,8 @@ class ADS7830(ADCDevice):
         self.address = 0x48 # 0x4b is the default i2c address for ADS7830 Module.   
         
     def analogRead(self, chn): # ADS7830 has 8 ADC input pins, chn:0,1,2,3,4,5,6,7
-        value = self.bus.read_byte_data(self.address, self.cmd|(((chn<<2 | chn>>1)&0x07)<<4))
+        try:
+            value = self.bus.read_byte_data(self.address, self.cmd|(((chn<<2 | chn>>1)&0x07)<<4))
+        except:
+            value = 0
         return value
