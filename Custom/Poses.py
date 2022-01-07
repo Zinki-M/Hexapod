799751c04d388f336ccc391c96ce548d2009251a
Zinki <z@a>
2022-01-07 21:55:35 +0100

    #IM try curlPosition

--- Custom/Poses.py
+++ Custom/Poses.py
@@ -7,6 +7,17 @@ def zeroPosition():
     Servo.servo_installation_position()
 
 
+def curledPosition():
+    S=Servo()     
+    for i in range(32):
+        if (i == 10 or i == 13 or i == 31):
+            S.setServoAngle(i,45)
+        elif (i == 18 or i == 21 or i == 27):
+            S.setServoAngle(i,135)
+        else:
+            S.setServoAngle(i,90)
+    time.sleep(3)
+
 
 
 if __name__ == '__main__':
