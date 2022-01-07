7302e964b3b7ca13f6955fbfa2b4c18f975f6cd5
Zinki <z@a>
2022-01-07 22:43:27 +0100

    #BF fix angles

--- Custom/Poses.py
+++ Custom/Poses.py
@@ -52,8 +52,8 @@ def zeroPosition():
 def tipToes():
     print("Standing on tiptoes")
     servo=Servo.Servo()
-    maxKnee = 60
-    maxTip = 60
+    maxKnee = 90
+    maxTip = 10
     try:
         
         servo.setServoAngle(15,90)
