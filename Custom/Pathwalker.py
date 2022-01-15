9af558b0e721b61c357aa9b28724832a0242c6cb
Zinki <Ischozar@gmail.com>
2022-01-15 17:54:36 +0100

    #IM turn around

---
+++ Custom/Pathwalker.py
@@ -0,0 +1,24 @@
+from os import sys, path
+sys.path.append(path.join(path.dirname(path.dirname(path.abspath(__file__))),"Server"))
+import Control
+
+
+#example:
+#data=['CMD_MOVE', '1', '0', '25', '10', '0']
+#Move command:'CMD_MOVE'
+#Gait Mode: "1"
+#Moving direction: x='0',y='25'
+#Delay:'10'
+#Action Mode : '0'   Angleless turn 
+
+def turnAround():
+    control = Control()
+    data = ['CMD_MOVE', "1", "0", "25", "10", "1"]
+    control.run(data)
+
+
+def makeMoveData():
+    data=['CMD_MOVE', '1', '0', '25', '10', '0']
+
+
+turnAround()
