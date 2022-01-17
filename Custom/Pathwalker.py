d632a57e006f297902b52487d5238e7e325d3a1e
Zinki <z@a>
2022-01-17 22:19:53 +0100

    #IM walk and sweep

--- Custom/Pathwalker.py
+++ Custom/Pathwalker.py
@@ -2,6 +2,8 @@ from os import sys, path
 sys.path.append(path.join(path.dirname(path.dirname(path.abspath(__file__))),"Server"))
 import Control
 import ADC
+import Servo
+import Ultrasonic
 import math
 import threading
 import time
@@ -15,6 +17,11 @@ import time
 #Action Mode : '0'   Angleless turn 
 
 STEPDEGREES = 28
+STEPDISTANCE = 10
+NORTH = [0,35]
+SOUTH = [0,-35]
+EAST = [35,0]
+WEST = [0,35]
 
 
 def turn(degrees):
@@ -37,10 +44,71 @@ def batteryPrint():
         time.sleep(3)
 
 
+def walk(distance, direction=NORTH):
+    """walk a distance in cm"""
+    control = Control.Control()
+    #one command is approximately ??? cm
+    commandsToRun = distance//STEPDISTANCE
+    remainder = (distance%STEPDISTANCE)
+    # # turnStrength max is 35
+    # turnStrength = math.floor(35 * (stepNum/commandsToRun))
+    data = ['CMD_MOVE', "1", str(direction[0]), str(direction[1]), "10", "0"]
+    print("Running",commandsToRun,"move commands in",direction)
+    for i in range(commandsToRun):
+        control.run(data)
+    #walk remainder
+    multiplier = STEPDISTANCE/remainder
+    data = ['CMD_MOVE', "1", str(math.floor(direction[0]*multiplier)), str(math.floor(direction[1]*multiplier)), "10", "0"]
+
+def sweep(low = 0, high = 180):
+    """sweep head"""
+    servo = Servo.Servo()
+    us = Ultrasonic.Ultrasonic()
+
+    servo.setServoAngle(0,90)
+    distanceSweep = []
+    for angle in range(high, low, -1):
+        servo.setServoAngle(1,angle)
+        distance = us.getDistance()
+        distanceSweep.append(distance)
+        time.sleep(0.01)
+    servo.setServoAngle(1,90)
+    return distanceSweep
+
+def fullSweep():
+    distances = []
+    for i in range(4):
+        distances.extend(sweep(45,135))
+        turn(90)
+    return distances
+
+def findMaxRangeAngle(full):
+    if(full):
+        distances = fullSweep()
+    else:
+        distances = sweep()
+    maxDistance = max(distances)
+    angle = distances.index(maxDistance)
+    return angle, maxDistance
+
+def walkToFarthestThing():
+    walkDist = 20
+    firstSweep = True
+    while True:
+        angle, dist = findMaxRangeAngle(firstSweep)
+        firstSweep = False
+        turn(angle)
+        if(dist < walkDist):
+            walk(dist)
+        else:
+            walk(walkDist)
+    
+
 RUNNING = True
 def run():
     x = threading.Thread(target=batteryPrint)
     x.start()
-    turn(1080)
-    RUNNING = False
+    walkToFarthestThing()
 
+run()
+RUNNING = False
